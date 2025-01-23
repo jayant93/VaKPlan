4f50d53119a6c5870fbb5250dc6db55d7c56419b
jayant.kumar <jayant.kumar@op.fi>
2025-01-23 16:34:04 +0530

    UserMicroservice for Vakplan

---
+++ Backend /user/src/test/java/com/vakPlan/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vakPlan.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
